3969cd48a40b66d33ca137b4790e45b740931f97
dhanesh0905 <dhaneshkash@gmail.com>
2025-06-20 14:26:23 +0200

    start

---
+++ dropshipping.py
@@ -0,0 +1,68 @@
+products = {
+    "men": [
+        {
+            "id": 101,
+            "name": "Naruto Uzumaki Jacket",
+            "price": 39.99,
+            "description": "Orange and black jacket with Uzumaki clan symbol",
+            "tags": ["Naruto", "Jacket", "Limited Edition"],
+            "image": "naruto_jacket.jpg"
+        },
+        {
+            "id": 102,
+            "name": "One Piece Jolly Roger Tee",
+            "price": 24.99,
+            "description": "Black cotton t-shirt with Straw Hat Pirates emblem",
+            "tags": ["One Piece", "T-Shirt", "Cotton"],
+            "image": "one_piece_tee.jpg"
+        },
+        {
+            "id": 103,
+            "name": "Dragon Ball Z Training Gi",
+            "price": 49.99,
+            "description": "Authentic Turtle School martial arts uniform",
+            "tags": ["DBZ", "Costume", "Orange"],
+            "image": "dbz_gi.jpg"
+        },
+        {
+            "id": 104,
+            "name": "Attack on Titan Survey Corps Hoodie",
+            "price": 44.99,
+            "description": "Green hoodie with Wings of Freedom emblem",
+            "tags": ["AOT", "Hoodie", "Warm"],
+            "image": "aot_hoodie.jpg"
+        }
+    ],
+    "women": [
+        {
+            "id": 201,
+            "name": "Sailor Moon Crystal Dress",
+            "price": 59.99,
+            "description": "Elegant navy blue dress with moon crescent details",
+            "tags": ["Sailor Moon", "Dress", "Elegant"],
+            "image": "sailor_dress.jpg"
+        },
+        {
+            "id": 202,
+            "name": "My Hero Academia UA Sweater",
+            "price": 34.99,
+            "description": "Red and white striped sweater with UA high logo",
+            "tags": ["MHA", "Sweater", "Comfy"],
+            "image": "mha_sweater.jpg"
+        },
+        {
+            "id": 203,
+            "name": "Demon Slayer Kimono Top",
+            "price": 42.99,
+            "description": "Patterned kimono-style top with butterfly motifs",
+            "tags": ["Kimetsu", "Top", "Traditional"],
+            "image": "demon_kimono.jpg"
+        },
+        {
+            "id": 204,
+            "name": "Jujutsu Kaisen Sukuna Cursed Leggings",
+            "price": 29.99,
+            "description": "Black leggings with cursed energy pattern details",
+            "tags": ["JJK", "Leggings", "Flexible"],
+            "image": "jjk_leggings.jpg"
+        }]}
